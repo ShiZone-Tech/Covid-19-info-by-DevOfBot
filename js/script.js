7ad79fba317bf3a4a56487747100dd9354666fa0
ShiZone-Tech <shizonetech@gmail.com>
2021-06-30 17:26:00 +0530

    completing scroll spy navbar & section to section with working

--- js/script.js
+++ js/script.js
@@ -13,5 +13,21 @@ $(document).ready(function() {
 		} else {
 			$('.scroll-top').hide();
 		}
+
+		// scroll spy
+
+		$('section').each(function() {
+
+			let height = $(this).height();
+			let offset = $(this).offset().top - 200;
+			let top = $(window).scrollTop();
+			let id = $(this).attr('id');
+			
+			if(top > offset && top < offset + height) {
+				$('.navbar ul li a').removeClass('active');
+				$('.navbar').find(`[href="#${id}"]`).addClass('active');
+			}
+
+		});
 	});
 });
